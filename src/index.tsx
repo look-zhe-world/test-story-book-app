c55669ae90be2a95033ab88dde350bb77ff3a02e
look-zhe-world <1015334621@qq.com>
2020-08-20 22:07:59 +0800

    clear

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,8 @@ export { default as Button } from "./components/button";
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
 export * from "./components/shared/animation";
+export * from "./components/icon";
+export * from "./components/avatar";
+export * from "./components/hightlight";
 
 // test coverall
